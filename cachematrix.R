9f3b7aacfccfcf00fe355a2decaaa1b8b73bcf99
Coco11 <xuezhang2007@yahoo.com>
2014-05-26 11:24:25 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -21,7 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The following function calculates the mean of the special "vector" created with the above function. However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+## The following function calculates the mean of the special "vector" created with the above function. 
+##However, it first checks to see if the mean has already been calculated. 
+##If so, it gets the mean from the cache and skips the computation. 
+##Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
   m <- x$getmean()
